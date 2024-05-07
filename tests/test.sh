8d930195bb2d133277fb0a44ec56d64c9bd4356e
andrzejnovak <novak5andrzej@gmail.com>
2024-05-06 21:22:07 -0400

    dpi100

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,5 @@
 P=0
-DPI=50
+DPI=100
 OPTS="--noroot"
 
 if [ "$1" = "A" ] || [ "$1" = "all" ]; then
@@ -8,13 +8,13 @@ if [ "$1" = "A" ] || [ "$1" = "all" ]; then
 fi
 
 if [ "$1" = "Abig" ] || [ "$1" = "all" ]; then
-combine_postfits --dpi $DPI -i fitDiags/fit_diag_Abig.root -o outs/plots_Abig_all --data --unblind  -p $P   $OP  
-combine_postfits --dpi $DPI -i fitDiags/fit_diag_Abig.root -o outs/plots_Abig --style styles/style_A.yml --data --unblind --sigs hcc,zcc --onto qcd --rmap zcc:z,hcc:r  --cats 'pass16:ptbin*pass2016;fail16:ptbin*fail2016;pass17:ptbin*pass2017;fail17:ptbin*fail2017;pass18:ptbin*pass2018;fail18:ptbin*fail2018;pass:ptbin*pass*;fail:ptbin*fail*;muCRpass16:muonCRpass2016;muCRfail16:muonCRfail2016;muCRpass17:muonCRpass2017;muCRfail17:muonCRfail2017;muCRpass18:muonCRpass2018;muCRfail18:muonCRfail2018' --bkgs top,other,wqq,wcq,zqq,zbb,hbb   --project-signal 200,0 -p $P   $OP
+    combine_postfits --dpi $DPI -i fitDiags/fit_diag_Abig.root -o outs/plots_Abig_all --data --unblind  -p $P   $OP  
+    combine_postfits --dpi $DPI -i fitDiags/fit_diag_Abig.root -o outs/plots_Abig --style styles/style_A.yml --data --unblind --sigs hcc,zcc --onto qcd --rmap zcc:z,hcc:r  --cats 'pass16:ptbin*pass2016;fail16:ptbin*fail2016;pass17:ptbin*pass2017;fail17:ptbin*fail2017;pass18:ptbin*pass2018;fail18:ptbin*fail2018;pass:ptbin*pass*;fail:ptbin*fail*;muCRpass16:muonCRpass2016;muCRfail16:muonCRfail2016;muCRpass17:muonCRpass2017;muCRfail17:muonCRfail2017;muCRpass18:muonCRpass2018;muCRfail18:muonCRfail2018' --bkgs top,other,wqq,wcq,zqq,zbb,hbb   --project-signal 200,0 -p $P   $OP
 fi
 
 if [ "$1" = "B" ] || [ "$1" = "all" ]; then
-combine_postfits --dpi $DPI -i fitDiags/fit_diag_B.root -o outs/plots_B_all --MC  -p $P   $OP  
-combine_postfits --dpi $DPI -i fitDiags/fit_diag_B.root --data --unblind -o outs/plots_B --style styles/style_B.yml  --xlabel 'Jet $m_{SD}$' --sigs b150,m150 --project-signals 2,2 --rmap 'm150:r_q,b150:r_b' --bkgs top,vlep,wqq,zqq,zbb,hbb --onto 2017_qcd --cats 'fail:ptbin*fail;passlow:ptbin*high*;passhigh:ptbin*passlow*'   -p $P   $OP
+    combine_postfits --dpi $DPI -i fitDiags/fit_diag_B.root -o outs/plots_B_all --MC  -p $P   $OP  
+    combine_postfits --dpi $DPI -i fitDiags/fit_diag_B.root --data --unblind -o outs/plots_B --style styles/style_B.yml  --xlabel 'Jet $m_{SD}$' --sigs b150,m150 --project-signals 2,2 --rmap 'm150:r_q,b150:r_b' --bkgs top,vlep,wqq,zqq,zbb,hbb --onto 2017_qcd --cats 'fail:ptbin*fail;passlow:ptbin*high*;passhigh:ptbin*passlow*'   -p $P   $OP
 fi
 
 if [ "$1" = "C" ] || [ "$1" = "all" ]; then

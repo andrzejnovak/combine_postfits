5b963c24f0f945b2247725f6aaf963afc2b1eb7e
Andrzej Novak <novak5andrzej@gmail.com>
2026-02-13 13:30:54 +0100

    fix: update pixi lock

--- src/combine_postfits/_version.py
+++ src/combine_postfits/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.1.dev97+g66d5c272d.d20260213"
-__version_tuple__ = version_tuple = (0, 1, "dev97", "g66d5c272d.d20260213")
+__version__ = version = "0.1.dev98+g12e9b2917.d20260213"
+__version_tuple__ = version_tuple = (0, 1, "dev98", "g12e9b2917.d20260213")
 
 __commit_id__ = commit_id = None
